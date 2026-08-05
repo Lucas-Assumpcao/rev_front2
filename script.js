24fda94f45d9da9a26b619cd0c34df0bc86c1f40
Lucas-Assumpcaogit config --global user.email assumpdev@gmail.comgit config --global user.name Lucas-Assumpcao <assumpdev@gmail.com>
2026-08-05 20:42:39 -0300

    carrossel_final

--- script.js
+++ script.js
@@ -38,13 +38,35 @@ function cadastro() {
 
 
     let botao = document.getElementById("meuBotao");
-
-    botao.addEventListener("mouseenter", function() {
-        botao.style.backgroundColor = "ultraviolet";
-        botao.style.color = "white";
-    });
+    if (botao) {
+        botao.addEventListener("mouseenter", function() {
+            botao.style.backgroundColor = "purple";
+            botao.style.color = "white";
+        });
 
         botao.addEventListener("mouseleave", function() {
-        botao.style.backgroundColor = "";
-        botao.style.color = "";
-    });
+            botao.style.backgroundColor = "";
+            botao.style.color = "";
+        });
+    }
+
+    const imagens = [
+        "https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/gettyimages-2202479142resized.webp?w=1600&h=1067",
+        "https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/12/vulcao-em-erupcao-e1735068190591.jpg?w=1200&h=900&crop=1",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_kUW4GvzzsprrB88JEdyuIhwrVUz2Fnpyg5rTQ6Mkjs4oK4_8pYo7vAc&s=10",
+        ]
+
+    const imgElemento = document.getElementById("imagem-carrossel");
+    let indiceAtual = 0;
+
+    function mudarFoto(){
+        indiceAtual = indiceAtual + 1;
+        
+        if(indiceAtual >= imagens.length){
+            indiceAtual = 0;
+        }
+
+        imgElemento.src = imagens[indiceAtual];
+    }
+
+    setInterval(mudarFoto, 3000);
